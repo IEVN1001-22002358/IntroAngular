fdc69492493c5c061b23be1b3fd05fde00592a1e
IEVN1001-22002358 <81339@alumnos.utleon.edu.mx>
2026-09-19 10:05:47 -0600

    Pipes 19/09/2026

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -1,8 +1,50 @@
 import { Component } from '@angular/core';
+import { IHeroes } from '../heroes';
 
 @Component({
   selector: 'app-heroes-list',
   standalone: false,
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+
+imageWidth:number=40;
+imageMargin:number=42;
+muestraImage:boolean=true;
+listfilter:string='';
+
+showImage():void{
+  this.muestraImage=!this.muestraImage;
+}
+
+  heroes:IHeroes[]=[
+    {
+    imagen:'https://dragonball-api.com/characters/picolo_normal.webp',
+    nombre:'Piccolo',
+    description:'Personaje',
+    race:'Namek',
+    ki:500000
+    },
+     {
+    imagen:'https://dragonball-api.com/characters/vegeta_normal.webp',
+    nombre:'Vegeta',
+    description:'Personaje base',
+    race:'Saiyan',
+    ki: 19.84
+    },
+     {
+    imagen:'https://dragonball-api.com/characters/Freezer.webp',
+    nombre:'Freezer',
+    description:'Villano',
+    race:'Frieza',
+    ki: 52.71
+    },
+     {
+    imagen:'https://dragonball-api.com/characters/celula.webp',
+    nombre:'Celula',
+    description:'Villano',
+    race:'Android',
+    ki: 5000
+    }
+  ]
+}
