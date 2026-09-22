912824a65c0b2820640fce9d0df67932ccad4b2b
IEVN1001-22002358 <81339@alumnos.utleon.edu.mx>
2026-09-21 20:59:00 -0600

    Distancia 21/09/2026

---
+++ src/app/formularios/distancia/distancia.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-distancia',
+  standalone: false,
+  templateUrl: './distancia.html',
+})
+export class Distancia {
+  
+}
